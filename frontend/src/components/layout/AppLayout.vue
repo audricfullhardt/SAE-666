06c6e1a9876fb74145fde050398bf8b3ce2aa2b3
Lucy Feneyrol <lucyfeneyrol@mac.home>
2026-06-18 22:03:56 +0200

    add global footer to layout

--- frontend/src/components/layout/AppLayout.vue
+++ frontend/src/components/layout/AppLayout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavBar from '@/components/layout/NavBar.vue'
+import FooterBar from '@/components/layout/FooterBar.vue'
 </script>
 
 <template>
   <div class="min-h-screen bg-menthe">
     <NavBar />
     <slot />
+    <FooterBar />
   </div>
 </template>
